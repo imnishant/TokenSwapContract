3aad111ea9fe8ec18eb7ec7ecf9ff6439402d21a
Nishant Sharma <theexplorer@Nishants-MacBook-Pro.local>
2021-01-14 22:46:43 +0530

    created DEX contract and added buy and sell function

---
+++ contracts/DEX.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./ERC20.sol";
+
+contract DEX {
+    
+    IERC20 public token;
+
+    event Bought(uint amount);
+    event Sold(uint amount);
+
+    constructor() public {
+        token = new ERC20("BChain Dynamics", "BCD");
+    }
+
+    function buy() payable public {
+        uint buyAmount = msg.value;
+        uint dexBalance = token.balanceOf(address(this));
+
+        require(buyAmount > 0, "Number of ether must be > 0");
+        require(buyAmount <= dexBalance, "Not enough tokens in the reserve");
+
+        token.transfer(msg.sender, buyAmount);
+        emit Bought(buyAmount);
+    }
+
+    function sell(uint amount) public {
+        require(amount > 0, "Number of token must be > 0");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        msg.sender.transfer(amount);
+        emit Sold(amount);
+    }
+}
